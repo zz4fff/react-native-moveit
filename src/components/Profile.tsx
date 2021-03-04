f802f9b22b6f2eb823d3d7a431ede0c310b6927a
Flávio Augusto de Freitas <zz4fff@gmail.com>
2021-03-04 16:29:29 -0300

    First commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+import { useContext } from 'react';
+
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/zz4fff.png" alt="Flávio Freitas" />
+      <div>
+        <strong>Flávio Augusto de Freitas</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+import { CountdownProvider } from '../contexts/CountdownContext';
+
+import '../styles/global.css';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  );
+}
+
+export default MyApp;
